78bfcbad9c49ad49198892e424b965c6dde772d0
AmareshMuddebihal <87269971+AmareshMuddebihal@users.noreply.github.com>
2021-10-25 09:11:15 +0530

    Add files via upload

---
+++ mobil numbertracker.py
@@ -0,0 +1,10 @@
+import phonenumbers
+from phonenumbers import carrier, geocoder, timezone
+
+mobileNo=input("Mobile no. with country code:")
+mobileNo=phonenumbers.parse(mobileNo)
+print(timezone.time_zones_for_number(mobileNo))
+print(carrier.name_for_number(mobileNo,"en"))
+print(geocoder.description_for_number(mobileNo,"en"))
+print("Valid Mobile Number:",phonenumbers.is_valid_number(mobileNo))
+print("Checking possibility of Number:",phonenumbers.is_possible_number(mobileNo))
